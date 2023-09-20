fa6a3a943c890e433c270e7c01caa74418f8a8e3
BalajiNilla <nillabalaji@gmail.com>
2023-09-20 13:01:53 +0530

    this is from sts without jwt token and security

---
+++ Assignment/src/main/java/com/assignment/project/config/JWTService.java
@@ -0,0 +1,11 @@
+package com.assignment.project.config;
+
+public class JWTService {
+
+
+	public String extractuserName(String token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
